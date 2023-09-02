d475f748940ac2f291fd8f7ec423f8d46d1121b8
Santiago Vanegas Mejia <125935085+santivanegas16@users.noreply.github.com>
2023-09-02 16:04:31 -0500

    fix navbar mobile (#5)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,7 +6,7 @@ export default function Home() {
     return (
         <div className="relative w-full min-h-screen top-[80px]">
             <img src={Banner2} alt="carousel" className="w-full h-[calc(100vh-80px)] object-cover" />
-            <div className="flex justify-around py-10 flex-wrap gap-10">
+            <div className="flex justify-around py-10 flex-wrap gap-10 px-2">
                 <img src={Banner1} alt="carousel" className="w-[300px] h-[300px] object-cover rounded-2xl" />
                 <img src={Banner1} alt="carousel" className="w-[300px] h-[300px] object-cover rounded-2xl" />
                 <img src={Banner1} alt="carousel" className="w-[300px] h-[300px] object-cover rounded-2xl" />
