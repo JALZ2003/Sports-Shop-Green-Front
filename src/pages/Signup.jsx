f8e90acbcc90f47de28ad7b03cbb85d10bffb13f
natigalo <natigalo1@gmail.com>
2023-09-02 19:46:08 -0500

    inputs

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -4,23 +4,39 @@ import arrow from '../../public/images/arrow.png'
 
 export default function Signup() {
   return (
-    <div className='w-full m-0 z-40'>
-      <section className='flex flex-row w-screen h-screen justify-center'>
-        <img src={arrow} alt="" className='absolute left-8 top-20'  />
-        <img className='w-1/2 mt-8 object-cover bg-blue-300' src={img} alt="marca" />
-        
-        <div className='bg-[#EDAF92] w-1/2 '>
-          <h1 className='mt-36 flex font-semibold justify-center'>CREATE AN ACCOUNT!</h1>
-          <form className='mt-12 flex flex-col justify-around px-12   h-32'>
-           <div className='flex justify-between'>
-            <label htmlFor="Name">Name</label> <input type="text" placeholder='Name' className='rounded-lg font-thin px-2 hover:border-2 border-black focus:border-black-400 active:bg-black-600'/>
-            <input type="text" placeholder='Name' className='rounded-lg font-thin'/>
-            </div> 
-            <div className=' flex justify-between '> 
-            <input type="text" placeholder='Name' className='rounded-lg font-thin' />
-            <input type="text" placeholder='Name' className='rounded-lg font-thin'/>
-              </div> 
+    <div className='w-screen m-0 z-40'>
+      <section className='flex  w-screen  h-screen justify-center'>
+        <img src={arrow} alt="" className='absolute left-4 top-16 w-8 h-4 ' />
+        <div className='w-1/2 h-screen   object-cover bg-blue-300 hidden md:flex md:items-center'><img src={img} alt="marca" /></div>
 
+        <div className='bg-[#EDAF92] h-full w-full md:w-1/2  flex flex-col  justify-center items-center'>
+          <h1 className='font-semibold md:-mt-64 lg:mt-20'>CREATE AN ACCOUNT!</h1>
+          <form className='mt-12 flex flex-col items-center justify-center h-80 '>
+
+            <div className='flex flex-row '>
+              <div className='flex flex-col justify-between w-40 mr-2'>
+                <label className='font-light text-sm ' htmlFor="Name">First Name</label>
+                <input type="text" className='rounded-lg mb-7 font-thin px-2 hover:border-2 border-orange-300 focus:border-black-400 active:bg-black-600  h-8' />
+                <label className='font-light text-sm' htmlFor="Name">Password</label>
+                <input type="text" className='rounded-lg font-thin px-2 hover:border-2 border-orange-300 focus:border-black-400 active:bg-black-600  h-8' />
+              </div>
+
+              <div className='flex  flex-col justify-between w-40 ml-2'>
+                <label className='font-light text-sm' htmlFor="Name">Last Name</label>
+                <input type="text" className='rounded-lg mb-7 font-thin px-2 hover:border-2 border-orange-300 focus:border-black-400 active:bg-black-600  h-8' />
+                <label className='font-light text-sm' htmlFor="Name">Confirme Password</label>
+                <input type="text" className='rounded-lg font-thin px-2 hover:border-2 border-orange-300 focus:border-black-400 active:bg-black-600  h-8' />
+              </div>
+            </div>
+
+            <div className='flex  flex-col justify-between w-40 ml-2 mt-8'>
+
+              <label className='font-light text-sm' htmlFor="Name">Confirme Password</label>
+              <input type="text" className='rounded-lg font-thin px-2 hover:border-2 border-orange-300 focus:border-black-400 active:bg-black-600  h-8' />
+
+            </div>
+            <label className='font-light text-[12px] w-3/4' htmlFor="Name"> <input type="checkbox" className='rounded-lg font-thin px-2 hover:border-2 border-black focus:border-black-400 active:bg-black-600  h-8' />I declare that I have read and authorize the use of my personal data according to the Authorization and accept the Terms and Conditions of Mometum X.</label>
+<input type="buttom" placeholder='REGISTER' className='bg-[#EC6B2F] hover:border-4 border-black'/>
           </form>
         </div>
       </section>
