dc89e5360921f11f7b57c989dbcc23bd5824bd56
andreaprada1993 <andrea_prada_93@hotmail.com>
2023-09-03 10:58:40 -0500

    front de login

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -9,13 +9,13 @@ export default function Signin() {
       </div>
       <div className=' bg-white w-[420px] h-[700px] z-10 mt-24 ml-5 xl:bg-opacity-70 xl:ml-[35%] xl:w-[30%]'>
         <div className='justify-items-centerer items-center relative lg:ml-2'>
-          <h1 className='font-sans text-4xl text-black text-center mt-14 lg:mt-14 '>Connect!</h1>
-          <h2 className=' text-black text-center mt-1 '>Enjoy all our products</h2>
+          <h1 className='font-sans text-5xl text-black text-center mt-14 lg:mt-14 '>Connect!</h1>
+          <h2 className=' text-black text-center mt-1 text-lg font-medium'>Enjoy all our products</h2>
           <div className='mt-12'>
             <h3 className=' text-black ml-8 font-medium' >Email</h3>
-            <input className='w-[85%] ml-8 h-8  bg-slate-300' />
+            <input className='w-[85%] ml-8 h-12  bg-slate-300 rounded-md ' />
             <h3 className=' text-black ml-8  mt-10 font-medium'>Password</h3>
-            <input className='w-[85%] ml-8 h-8 bg-slate-300' />
+            <input className='w-[85%] ml-8 h-12 bg-slate-300 rounded-md' />
           </div>
           <div className='mt-9'>
             <input className=' bg-orange-600 bg-current rounded-full w-[60%] h-12 ml-[20%] text-black font-bold  lg:ml-[20%]'
