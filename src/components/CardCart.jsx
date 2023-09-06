067ebac54ff7af7407e771a110eb7d2161e8f844
Gerardo  Aguirre <rg.aguirre93@gmail.com>
2023-09-06 18:17:53 -0500

    encabezado y correccion de espaciados

--- src/components/CardCart.jsx
+++ src/components/CardCart.jsx
@@ -1,7 +1,7 @@
 import item from "/Images/item.png"
 export default function CardCart() {
     return (
-        <div className="hover:cursor-pointer flex justify-between mt-4 bg-[#EDF2F4] md:w-[80%] lg:w-[600px] xl:w-[650px] 2xl:w-[60%]  h-48 md:h-52 border-[1px] border-[#000000]">
+        <div className="hover:cursor-pointer transition hover:scale-105 flex justify-between mt-4 bg-[#EDF2F4] w-full md:w-[80%] lg:w-[600px] xl:w-[650px] 2xl:w-[750px] h-48 md:h-52 border-[1px] border-[#000000]">
             <div className="h-full lg:w-1/3 w-[300px] border-r-[1px] border-[#000000]">
                 <img src={item} alt="" className="h-full w-full object-cover object-center" />
             </div>
