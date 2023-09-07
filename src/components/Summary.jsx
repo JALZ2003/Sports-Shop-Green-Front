0f95a637c09b42d84628726fcf997f945ee9bb11
Gerardo  Aguirre <95187790+Vazhh@users.noreply.github.com>
2023-09-07 11:41:42 -0500

    Cart (#19)
    * Create card of cart
    
    * fix
    
    * summary
    
    * encabezado y correccion de espaciados
    
    * fix

---
+++ src/components/Summary.jsx
@@ -0,0 +1,28 @@
+import { Link as Anchor } from "react-router-dom"
+import arrow from "/Images/arrowSummary.png"
+
+export default function Summary() {
+    return (
+        <div className="w-[120%]">
+            <Anchor to={"/shop"} className="bg-blue text-white flex items-center w-full justify-between h-9 p-3 hover:scale-105">PAY YOUR ORDER! <img src={arrow} className="w-10 h-3 ms-4" alt="" /></Anchor>
+            <h1 className="font-bold mt-2">ORDER SUMMARY</h1>
+            <div className="flex justify-between mt-2">
+                <p>2 products</p>
+                <p>$40</p>
+            </div>
+            <div className="flex justify-between mt-1">
+                <p>Shipment</p>
+                <p>Free</p>
+            </div>
+            <div className="border border-solid border-gray-400 mt-2" ></div>
+            <div className="flex justify-between mt-2">
+                <p>Total</p>
+                <p>$40</p>
+            </div>
+            <p className="bg-gray-100 p-6 mt-4 text-justify">FREE SHIPPING FOR MEMBERS + EXPRESS DELIVERY
+                Until September 30, take advantage of express delivery for $14. Receive your order the next business day. Applies to orders with confirmed payment until 11:59 pm. Orders with confirmed payment on Fridays after 11:59 pm, will be prepared the following Monday for delivery in 24 hours.
+                Also, if you are a member, take advantage of free shipping until September 18 on all your purchases, log in or register to take advantage.</p>
+        </div>
+
+    )
+}
