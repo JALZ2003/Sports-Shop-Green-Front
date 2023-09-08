2bb1fbf099fba518f8403984ca6aa1d59e6e59b5
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-09-07 22:48:53 -0500

    fix signin and add panel admin

--- src/pages/AdminCategories.jsx
+++ src/pages/AdminCategories.jsx
@@ -2,8 +2,11 @@ import React from 'react'
 
 export default function AdminCategories() {
   return (
-    <div className='relative md:left-[330px] lg:left-[430px] top-[40px]'>
-    <div className='lg:w-[calc(100%-430px)] md:w-[calc(100%-330px)] flex justify-center text-[40px] font-bold'>Categories</div>
-</div>
+    <div className='flex w-full h-screen'>
+      <div className='lg:w-[600px]'></div>
+      <div className='w-full flex flex-col items-center pt-[180px] lg:pt-[50px]'>
+        <h1 className='text-[40px] font-bold'>Categories</h1>
+      </div>
+    </div>
   )
 }
--- src/pages/Panel.jsx
+++ src/pages/Panel.jsx
@@ -1,10 +1,27 @@
-import React from 'react'
+import logo from "/Images/LogoCorto.png"
 
 export default function Panel() {
   return (
-    <div className='flex'>
-      <div className='relative left-0 md:w-[330px] lg:w-[430px] md:top-[40px] max-[445px]:top-[220px] top-[180px]'></div>
-      <div className='w-full flex justify-center text-[40px] font-bold'>Panel</div>
+    <div className='flex w-full h-screen'>
+      <div className='lg:w-[600px]'></div>
+      <div className='w-full flex flex-col items-center pt-[180px] lg:pt-[80px]'>
+        <h1 className='text-[40px] font-bold'>Panel</h1>
+        <div className='py-5 xl:py-10 grid grid-cols-1 xl:grid-cols-3 w-full md:w-[65%] justify-items-center gap-7'>
+          <div className='rounded-full w-[200px] h-[200px] bg-slate-200 p-2 flex flex-col justify-center items-center'>
+            <h1 className='font-bold text-[30px]'>Users</h1>
+            <p className='text-[24px]'>50</p>
+          </div>
+          <div className='rounded-full w-[200px] h-[200px] bg-yellow-200 p-2 flex flex-col justify-center items-center'>
+            <h1 className='font-bold text-[30px]'>Products</h1>
+            <p className='text-[24px]'>2500</p>
+          </div>
+          <div className='rounded-full w-[200px] h-[200px] bg-green-200 p-2 flex flex-col justify-center items-center'>
+            <h1 className='font-bold text-[30px]'>Categories</h1>
+            <p className='text-[24px]'>8</p>
+          </div>
+        </div>
+        <img className='w-[320px] h-[180px]' src={logo}></img>
+      </div>
     </div>
   )
 }
