e8907fc8e20696f6de1f1f66ff993d2ddd032c41
santivanegas16 <santiago.vanegasm16@gmail.com>
2023-09-07 20:42:39 -0500

    fix panel width

--- src/pages/Panel.jsx
+++ src/pages/Panel.jsx
@@ -2,8 +2,9 @@ import React from 'react'
 
 export default function Panel() {
   return (
-    <div className='relative md:left-[330px] lg:left-[430px] md:top-[40px] max-[445px]:top-[220px] top-[180px]'>
-        <div className='lg:w-[calc(100%-430px)] md:w-[calc(100%-330px)] flex justify-center text-[40px] font-bold'>Panel</div>
+    <div className='flex'>
+      <div className='relative left-0 md:w-[330px] lg:w-[430px] md:top-[40px] max-[445px]:top-[220px] top-[180px]'></div>
+      <div className='w-full flex justify-center text-[40px] font-bold'>Panel</div>
     </div>
   )
 }
