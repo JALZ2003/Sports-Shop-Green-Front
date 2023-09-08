58d4bf8b4913391e8781bf3cdca250d55775d0ee
and1kmpo <acampolpez@gmail.com>
2023-09-08 11:59:32 -0500

    pagination shop

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,26 +1,45 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import Sidebar from "../components/Sidebar";
 import Card from "../components/Card";
 import SearchBar from "../components/SearchBar";
+import Pagination from "../components/Pagination";
 import apiUrl from "../../apiUrl"
 
 function Shop() {
     const [products, setProducts] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [hasPrevPage, setHasPrevPage] = useState(false);
+    const [hasNextPage, setHasNextPage] = useState(false);
 
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch(apiUrl + "/products");
+                const response = await fetch(apiUrl + `/products?page=${currentPage}`);
                 const data = await response.json();
                 setProducts(data.response);
-                console.log(data);
+                setTotalPages(data.pages);
+                setHasPrevPage(data.prev !== null);
+                setHasNextPage(data.next !== null);
             } catch (error) {
                 console.error('Error al obtener datos:', error);
             }
         }
 
         fetchData();
-    }, []);
+    }, [currentPage]);
+
+    const goToPrevPage = () => {
+        if (hasPrevPage) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const goToNextPage = () => {
+        if (hasNextPage) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
 
     return (
         <div className="min-h-screen flex flex-col mt-16">
@@ -32,8 +51,17 @@ function Shop() {
                         {products.map(product => (
                             <Card key={product._id} product={product} />
                         ))}
-
                     </div>
+
+                    {/* Pagination */}
+                    <Pagination
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        goToPrevPage={goToPrevPage}
+                        goToNextPage={goToNextPage}
+                        hasPrevPage={hasPrevPage}
+                        hasNextPage={hasNextPage}
+                    />
                 </div>
             </main>
         </div>
