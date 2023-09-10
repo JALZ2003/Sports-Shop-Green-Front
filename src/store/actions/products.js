822ab277fe59b74f3eb7ab9b79d34e04e6479c14
Santiago Vanegas Mejia <125935085+santivanegas16@users.noreply.github.com>
2023-09-10 17:06:45 -0500

    destroy and create products ok (#35)

--- src/store/actions/products.js
+++ src/store/actions/products.js
@@ -1,4 +1,7 @@
-import { createAction} from "@reduxjs/toolkit";
+import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+import apiUrl from "../../apiUrl"
+import headers from "../../header"
 
 const save_name = createAction('save_name', obj => {
     return { payload: { name: obj.name} }
@@ -8,9 +11,24 @@ const save_checks = createAction('save_checks', obj => {
     return { payload: { checks: obj.checks } }
 })
 
+const destroyProduct = createAsyncThunk(
+    "destroyProduct",
+    async (obj) => {
+        try {
+            let one = await axios.delete(apiUrl + "products/" + obj.product_id, headers())
+            return {
+                id_to_delete: one.data.response 
+            }
+        } catch (error) {
+            return null
+        }
+    }
+)
+
 const productsActions = {
     save_name,
-    save_checks
+    save_checks,
+    destroyProduct
 }
 
 export default productsActions
