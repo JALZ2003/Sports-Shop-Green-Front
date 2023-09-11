9f3f925c1fa72ce83f7623d070dce1473f20f6d1
JALZ <jannus.largo@correounivalle.edu.co>
2023-09-11 18:53:25 -0500

    Product axios add

--- src/components/CardCart.jsx
+++ src/components/CardCart.jsx
@@ -1,24 +1,34 @@
+import { useEffect, useState } from "react";
 import item from "/Images/item.png"
-export default function CardCart() {
+export default function CardCart({ name, price, stock, image, quantity }) {
+
+    const [options, setOptions] = useState([]);
+
+    useEffect(() => {
+        let positions = []
+        for (let i = 0; i < stock; i++) {
+            positions.push(i);
+        }
+        setOptions(positions);
+    }, [])
+
+    console.log(options);
+
     return (
         <div className="hover:cursor-pointer transition hover:scale-105 flex justify-between mt-4 bg-[#EDF2F4]  w-[99%] md:w-[80%] lg:w-[600px] xl:w-[650px] 2xl:w-[750px] h-48 md:h-52 border-[1px] border-[#000000]">
             <div className="h-full lg:w-1/3 w-[300px] border-r-[1px] border-[#000000]">
-                <img src={item} alt="" className="h-full w-full object-cover object-center" />
+                <img src={image} alt="" className="h-full w-full object-cover object-center" />
             </div>
             <div className="flex flex-col justify-around ms-8 md:ms-7 lg:mt-4 l lg:w-[45%] w-[70%] mb-5 mr-10 md:mr-14">
                 <div className="flex flex-col sm:flex-row justify-between md:text-[25px] xl:text-[25px] lg:text-[20px] ">
-                    <p>Name Product</p>
-                    <p className="mt-[1px]">Price: $20</p>
+                    <p> {name} </p>
+                    <p className="mt-[1px]">Price: ${price}</p>
                 </div>
-                <p className="lg:text-[20px] lg:w-[50vw]">In Stock</p>
+                <p className="lg:text-[20px] lg:w-[50vw]"> {stock > 0 ? "In Stock" : "Not Stock"} </p>
                 <div>
-                    
+
                     <select id="cantidad" className="px-2 py-1 border-[1px] rounded-md border-[#000000]">
-                       
-                        <option value="1">1</option>
-                        <option value="2">2</option>
-                        <option value="3">3</option>
-                        
+                        { options.map(each => <option key={each} value={each} selected={each==quantity}> {each} </option>) }
                     </select>
                 </div>
                 {/* <input type="number" name="" id="" defaultValuevalue="1" className="md:w-16 md:h-10 w-9 h-6 rounded-sm text-center border-[1px] border-[#000000]" /> */}
--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,30 +1,54 @@
+import { useEffect, useState } from "react"
 import CardCart from "../components/CardCart"
 import Summary from "../components/Summary"
 import { Link as Anchor } from "react-router-dom"
+import axios from 'axios';
+import apiUrl from '../apiUrl';
+import headers from '../header';
 
 export default function Cart() {
+
+    const [cart, setCart] = useState([]);
+    const [total, setTotal] = useState(0);
+
+    useEffect(() => {
+        axios(`${apiUrl}carts`, headers()).then(res => {
+            setCart(res.data.response.cart);
+            setTotal(res.data.response.total);
+        }).catch(error => {
+            setCart([]);
+            setTotal(0);
+        })
+    }, [])
+
+    console.log(cart);
+
     return (
         <div className="flex flex-col w-full lg:w-[70%] mt-[148px] items-center justify-center mb-8 z-10 ">
 
-            
-                <div className="flex flex-col justify-between ps-10 pr-10 md:p-0 w-full md:w-[80%] lg:w-[600px] xl:w-[650px] 2xl:w-[750px] lg:items-start">
-                    <div className="flex justify-between items-center w-full ">
-                        <h1 className="font-bold md:text-3xl text-xl">YOUR CART</h1>
-                        <Anchor to={"/shop"} className="bg-blue text-white h-9 w-[200px] flex items-center justify-center ms-3 text-center rounded-sm">CONTINUE SHOPPING</Anchor>
-                    </div>
-                    <p className="mt-6">The items in your cart are not reserved. Complete the checkout process now to get them.</p>
+
+            <div className="flex flex-col justify-between ps-10 pr-10 md:p-0 w-full md:w-[80%] lg:w-[600px] xl:w-[650px] 2xl:w-[750px] lg:items-start">
+                <div className="flex justify-between items-center w-full ">
+                    <h1 className="font-bold md:text-3xl text-xl">YOUR CART</h1>
+                    <Anchor to={"/shop"} className="bg-blue text-white h-9 w-[200px] flex items-center justify-center ms-3 text-center rounded-sm">CONTINUE SHOPPING</Anchor>
                 </div>
-                <div className="border border-solid border-gray-400 mt-2 w-[90%] lg:hidden" ></div>
-                <div className="lg:absolute flex items-center justify-center lg:right-[4%] xl:right-[13%] lg:top-32 lg:w-[257px] xl:w-[300px] 2xl:w-[400px] w-[80%] mt-5 mb-5">
-                    <Summary />
-                </div>
-                <div className="border border-solid border-gray-400 mt-2 w-[90%] lg:hidden" ></div>
-                <div className="md:border-[1px] pt-5 w-full md:w-[80%] lg:w-[650px] xl:w-[700px] 2xl:w-[800px] mt-4">
+                <p className="mt-6">The items in your cart are not reserved. Complete the checkout process now to get them.</p>
+            </div>
+            <div className="border border-solid border-gray-400 mt-2 w-[90%] lg:hidden" ></div>
+            <div className="lg:absolute flex items-center justify-center lg:right-[4%] xl:right-[13%] lg:top-32 lg:w-[257px] xl:w-[300px] 2xl:w-[400px] w-[80%] mt-5 mb-5">
+                <Summary quantityProducts={cart.length} total={total} />
+            </div>
+            <div className="border border-solid border-gray-400 mt-2 w-[90%] lg:hidden" ></div>
+            <div className="md:border-[1px] pt-5 w-full md:w-[80%] lg:w-[650px] xl:w-[700px] 2xl:w-[800px] mt-4">
                 <div className="flex flex-col  w-full items-center mt-4 mb-8">
-                    <CardCart />
-                    <CardCart />
-                    <CardCart />
-                    <CardCart />
+                    {cart.map((product, index) => <CardCart
+                        key={index}
+                        stock={product?.product_id?.stock}
+                        image={product?.product_id?.url_photo}
+                        name={product?.product_id?.name}
+                        price={product?.product_id?.price}
+                        quantity={product?.quantity}
+                    />)}
                 </div>
             </div>
 
