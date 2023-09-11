c559bda4da81ae12835910bfa454230edfbbbfb1
and1kmpo <acampolpez@gmail.com>
2023-09-11 17:24:54 -0500

    filters by category and price

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,92 +1,94 @@
 import { useState } from "react";
-import {
-    RiMoneyDollarCircleLine,
-    RiFilter3Line,
-    RiCloseLine,
-} from "react-icons/ri";
+import { RiMoneyDollarCircleLine, RiFilter3Line, RiCloseLine } from "react-icons/ri";
 
-const Sidebar = ({ categories }) => {
+const Sidebar = ({ categories, filteredCategories, onCategoryChange, onPriceFilterApply }) => {
     const [showSidebar, setShowSidebar] = useState(false);
-    const [checkedCategories, setCheckedCategories] = useState({});
+    const [minPrice, setMinPrice] = useState("");
+    const [maxPrice, setMaxPrice] = useState("");
 
-    const handleCheckboxChange = (categoryId) => {
-        setCheckedCategories(prevState => ({
-            ...prevState,
-            [categoryId]: !prevState[categoryId]
-        }));
+    const handleCheckboxChange = (categoryId, isChecked) => {
+        onCategoryChange(categoryId, isChecked);
     };
 
-    const handleLabelClick = (color) => {
-        console.log(color);
-    };
+    const handleMinPriceChange = (e) => {
+        setMinPrice(e.target.value);
+    }
+
+    const handleMaxPriceChange = (e) => {
+        setMaxPrice(e.target.value);
+    }
+
+    const handleFilterApply = () => {
+        const priceFilter = {
+            minPrice: parseFloat(minPrice),
+            maxPrice: parseFloat(maxPrice)
+        };
+        onPriceFilterApply(priceFilter);
+    }
 
     return (
         <>
             <div className={`w-[80%] sm:w-[40%] lg:max-w-[300px] z-20 fixed top-0  lg:static ${showSidebar ? "left-0 " : "-left-full "
-                } h-full lg:h-auto text-white transition-all bg-blue p-4 lg:p-0 lg:shadow-none ${showSidebar ? "lg:rounded-lg" : "rounded-lg"
+                } h-full lg:h-auto text-white transition-all bg-blue p-4 lg:p-4 lg:shadow-none ${showSidebar ? "lg:rounded-lg" : "rounded-lg"
                 }`}
             >
-
-                {/* Search */}
-                <div className=" rounded-2xl p-4 mb-4 z-20">
-                    <h4 className="my-4 text-white text-lg font-semibold">Categories</h4>
-                    <div className="flex flex-col gap-2">
-
-                        {categories.map(category => (
-                            <div
-                                key={category._id}
-                                className={`flex flex-col justify-between bg-slate-950`}
-                                style={{ backgroundColor: checkedCategories[category._id] ? category.color : '' }}
-                            >
-                                <input
-                                    type="checkbox"
-                                    id={category._id}
-                                    value={category._id}
-                                    className={`appearance-none h-1 w-full bg-gray-400 transition-all duration-300`}
-                                    checked={checkedCategories[category._id] || false}
-                                    onChange={() => handleCheckboxChange(category._id)}
-                                />
-                                <label
-                                    htmlFor={category._id}
-                                    className='p-1 flex flex-row justify-center px-2 select-none text-white'
-                                    onClick={() => handleLabelClick(category.color)}
-                                >
-                                    {category.name}
-                                </label>
-                            </div>
-                        ))}
-
-                    </div>
-
-                    <h4 className="my-4 text-white text-lg font-semibold">Price</h4>
-                    <form className="flex flex-col gap-8">
-                        <div className="flex items-center justify-between gap-4">
-                            <div className="relative">
-                                <RiMoneyDollarCircleLine className="absolute left-2 top-1/2 -translate-y-1/2" />
-                                <input
-                                    type="number"
-                                    className="bg-[#181A20] py-2 pl-8 pr-4 rounded-xl outline-none w-full"
-                                />
-                            </div>
-                            <span>-</span>
-                            <div className="relative">
-                                <RiMoneyDollarCircleLine className="absolute left-2 top-1/2 -translate-y-1/2" />
-                                <input
-                                    type="number"
-                                    className="bg-[#181A20] py-2 pl-8 pr-4 rounded-xl outline-none w-full"
-                                />
-                            </div>
-                        </div>
-                        <button
-                            type="submit"
-                            className="bg-orange text-black font-bold rounded-full w-full p-3 hover:-translate-y-1 transition-all duration-200"
+                <h4 className="my-4 text-white text-lg font-semibold">Categories</h4>
+                <div className="flex flex-col gap-2">
+                    {categories.map(category => (
+                        <div
+                            key={category._id}
+                            className={`flex flex-col justify-between bg-slate-950`}
+                            style={{ backgroundColor: filteredCategories[category._id] ? category.color : '' }}
                         >
-                            Apply Filter
-                        </button>
-                    </form>
+                            <input
+                                type="checkbox"
+                                id={category._id}
+                                value={category._id}
+                                className={`appearance-none h-1 w-full bg-gray-400 transition-all duration-300`}
+                                checked={filteredCategories[category._id] || false}
+                                onChange={(e) => handleCheckboxChange(category._id, e.target.checked)}
+                            />
+                            <label
+                                htmlFor={category._id}
+                                className='p-1 flex flex-row justify-center px-2 select-none text-white'
+                            >
+                                {category.name}
+                            </label>
+                        </div>
+                    ))}
                 </div>
+                <h4 className="my-4 text-white text-lg font-semibold">Price</h4>
+                <div className="flex items-center justify-between gap-4 mb-4">
+                    <div className="relative">
+                        <RiMoneyDollarCircleLine className="absolute left-2 top-1/2 -translate-y-1/2" />
+                        <input
+                            type="number"
+                            placeholder="Min"
+                            value={minPrice}
+                            onChange={handleMinPriceChange}
+                            className="bg-[#181A20] py-2 pl-8 pr-4 rounded-xl outline-none w-full"
+                        />
+                    </div>
+                    <span>-</span>
+                    <div className="relative">
+                        <RiMoneyDollarCircleLine className="absolute left-2 top-1/2 -translate-y-1/2" />
+                        <input
+                            type="number"
+                            placeholder="Max"
+                            value={maxPrice}
+                            onChange={handleMaxPriceChange}
+                            className="bg-[#181A20] py-2 pl-8 pr-4 rounded-xl outline-none w-full"
+                        />
+                    </div>
+                </div>
+                <button
+                    type="button"
+                    onClick={handleFilterApply}
+                    className="bg-orange text-black font-bold rounded-full w-full p-3 hover:-translate-y-1 transition-all duration-200"
+                >
+                    Apply Filter
+                </button>
             </div>
-            {/* Button mobile */}
             <button
                 onClick={() => setShowSidebar(!showSidebar)}
                 className="lg:hidden fixed bottom-4 right-4 bg-orange p-4 rounded-full text-xl"
--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Sidebar from "../components/Sidebar";
@@ -12,96 +11,107 @@ function Shop() {
 
     const [products, setProducts] = useState([]);
     const [categories, setCategories] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(1);
+    const [hasPrevPage, setHasPrevPage] = useState(false);
+    const [hasNextPage, setHasNextPage] = useState(false);
+    const [filteredCategories, setFilteredCategories] = useState({});
+    const [priceFilter, setPriceFilter] = useState({});
 
-
-    function Shop() {
-        const [products, setProducts] = useState([]);
-        const [currentPage, setCurrentPage] = useState(1);
-        const [totalPages, setTotalPages] = useState(1);
-        const [hasPrevPage, setHasPrevPage] = useState(false);
-        const [hasNextPage, setHasNextPage] = useState(false);
-
-        useEffect(() => {
-            const fetchData = async () => {
-                try {
-                    const response = await fetch(apiUrl + `/products?page=${currentPage}`);
-                    const data = await response.json();
-                    setProducts(data.response);
-                    setTotalPages(data.pages);
-                    setHasPrevPage(data.prev !== null);
-                    setHasNextPage(data.next !== null);
-                } catch (error) {
-                    console.error('Error getting data:', error);
-                }
-            }
-
-            fetchData();
-        }, [currentPage]);
-
-        useEffect(() => {
-            const fetchCategories = async () => {
-                try {
-                    const response = await fetch(apiUrl + '/categories');
-                    const data = await response.json();
-
-                    // Puedes acceder a las propiedades de las categorías aquí
-                    console.log(data.response);
-
-                    // Actualiza el estado de las categorías
-                    setCategories(data.response);
-                } catch (error) {
-                    console.error('Error al obtener categorías:', error);
-                }
-            };
-
-            fetchCategories();
-        }, []);
-
-        useEffect(() => {
-            // Update URL with useNavigate
-            navigate(`/products?page=${currentPage}`);
-        }, [currentPage, navigate]);
-
-
-        const goToPrevPage = () => {
-            if (hasPrevPage) {
-                setCurrentPage(currentPage - 1);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(apiUrl + `/products?page=${currentPage}`);
+                const data = await response.json();
+                setProducts(data.response);
+                setTotalPages(data.pages);
+                setHasPrevPage(data.prev !== null);
+                setHasNextPage(data.next !== null);
+            } catch (error) {
+                console.error('Error getting data:', error);
             }
         }
 
-        const goToNextPage = () => {
-            if (hasNextPage) {
-                setCurrentPage(currentPage + 1);
+        fetchData();
+    }, [currentPage]);
+
+    useEffect(() => {
+        const fetchCategories = async () => {
+            try {
+                const response = await fetch(apiUrl + '/categories');
+                const data = await response.json();
+
+                setCategories(data.response);
+            } catch (error) {
+                console.error('Error al obtener categorías:', error);
             }
-        }
+        };
 
-        return (
-            <div className="min-h-screen flex flex-col mt-16">
-                <main className="flex gap-8 p-8 pt-0 mt-10">
-                    <Sidebar categories={categories} />
-                    <div className="flex-1 h-full flex flex-col justify-center">
-                        <SearchBar />
+        fetchCategories();
+    }, []);
 
-                        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
-                            {products.map(product => (
-                                <Card key={product._id} product={product} />
-                            ))}
-                        </div>
-
-                        {/* Pagination */}
-                        <Pagination
-                            currentPage={currentPage}
-                            totalPages={totalPages}
-                            goToPrevPage={goToPrevPage}
-                            goToNextPage={goToNextPage}
-                            hasPrevPage={hasPrevPage}
-                            hasNextPage={hasNextPage}
-                        />
-                    </div>
-                </main>
-            </div>
-        );
+    const handleCategoryChange = (categoryId, isChecked) => {
+        setFilteredCategories(prevState => ({
+            ...prevState,
+            [categoryId]: isChecked
+        }));
     }
+
+    const handlePriceFilterApply = (priceFilter) => {
+        setPriceFilter(priceFilter);
+    }
+
+    const goToPrevPage = () => {
+        if (hasPrevPage) {
+            setCurrentPage(currentPage - 1);
+        }
+    }
+
+    const goToNextPage = () => {
+        if (hasNextPage) {
+            setCurrentPage(currentPage + 1);
+        }
+    }
+
+    return (
+        <div className="min-h-screen flex flex-col mt-16">
+            <main className="flex gap-8 p-8 pt-0 mt-10">
+                <Sidebar
+                    categories={categories}
+                    filteredCategories={filteredCategories}
+                    onCategoryChange={handleCategoryChange}
+                    onPriceFilterApply={handlePriceFilterApply}
+                />
+                <div className="flex-1 h-full flex flex-col justify-center">
+                    <SearchBar />
+                    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
+                        {products
+                            .filter(product => {
+                                const productCategoryId = product.category_id._id;
+                                const isCategorySelected = filteredCategories[productCategoryId] || Object.values(filteredCategories).every(value => !value);
+                                const isPriceInRange = (
+                                    (!priceFilter.minPrice || product.price >= priceFilter.minPrice) &&
+                                    (!priceFilter.maxPrice || product.price <= priceFilter.maxPrice)
+                                );
+                                return isCategorySelected && isPriceInRange;
+                            })
+                            .map(product => (
+                                <Card key={product._id} product={product} />
+                            ))
+                        }
+                    </div>
+                    <Pagination
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        goToPrevPage={goToPrevPage}
+                        goToNextPage={goToNextPage}
+                        hasPrevPage={hasPrevPage}
+                        hasNextPage={hasNextPage}
+                    />
+                </div>
+            </main>
+        </div>
+    );
 }
 
 export default Shop;
