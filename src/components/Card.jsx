eec3381a45bbfd658731c4e103ce46670f110a6f
and1kmpo <acampolpez@gmail.com>
2023-09-12 16:59:11 -0500

    adding button fav

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,20 @@
-import { RiShoppingCart2Line } from "react-icons/ri";
+import { RiShoppingCart2Line, RiHeartLine } from "react-icons/ri";
 
 const Card = ({ product }) => {
     const categoryColor = product.category_id.color || '#000';
 
     return (
         <div className={`bg-blue-200 p-1 md:p-2 lg:p-3 rounded-lg shadow-md border-l-4  border-gray-800/10`} style={{ borderColor: categoryColor }}>
-            <img
-                src={product.url_photo}
-                className="w-full h-48 md:h-56 lg:h-64 object-cover rounded-lg border border-gray-300"
-                alt={product.name}
-            />
+            <div className="relative">
+                <img
+                    src={product.url_photo}
+                    className="w-full h-48 md:h-56 lg:h-64 object-cover rounded-lg border border-gray-300"
+                    alt={product.name}
+                />
+                <button className="absolute top-2 right-2 bg-red-500/90 text-white font-bold rounded-full p-1 md:p-2 lg:p-3 hover:bg-red-600 hover:scale-110 transition-all duration-200 shadow-black shadow-md">
+                    <RiHeartLine />
+                </button>
+            </div>
             <div className="p-4 flex flex-col">
                 <h1 className="text-base md:text-lg lg:text-xl text-blue-700 font-semibold capitalize h-14" title={product.name}>{product.name}</h1>
                 <div className=" flex justify-between">
@@ -18,7 +23,8 @@ const Card = ({ product }) => {
                 </div>
                 <div className="flex items-center justify-between mt-4">
                     <h5 className="text-lg md:text-xl lg:text-2xl text-orange">${product.price}</h5>
-                    <button className="bg-orange text-black font-bold rounded-full p-1 md:p-2 lg:p-3 hover:bg-orange-600 hover:-translate-y-1 transition-all duration-200 shadow-black shadow-md ">
+
+                    <button className="bg-orange text-black font-bold rounded-full p-1 md:p-1 lg:p-2 hover:bg-orange-600 hover:-translate-y-1 transition-all duration-200 shadow-black shadow-md ">
                         <RiShoppingCart2Line />
                     </button>
                 </div>
