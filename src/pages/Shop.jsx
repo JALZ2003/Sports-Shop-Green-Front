9ca25961d5b6ceb18cd903aa634f304f98997d77
Andy Campo <acampolpez@gmail.com>
2023-09-12 14:58:30 -0500

    Filtering (#53)
    * filtering term, category, price
    
    * filtering fix
    
    * add message not found products
    
    * improving style cards and sidebar

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -77,32 +77,44 @@ function Shop() {
                     onPriceFilterApply={handlePriceFilterApply}
                     onSearch={handleSearchChange}
                 />
-                <div className="flex-1 flex flex-col justify-center items-center">
-                    {products
-                        .filter(product => {
-                            const productCategoryId = product.category_id._id;
-                            const isCategorySelected = filteredCategories[productCategoryId] || Object.values(filteredCategories).every(value => !value);
-                            const isPriceInRange = (
-                                (!priceFilter.minPrice || product.price >= priceFilter.minPrice) &&
-                                (!priceFilter.maxPrice || product.price <= priceFilter.maxPrice)
-                            );
-                            const isTextMatch = (
-                                !searchTerm ||
-                                product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
-                                product.description.toLowerCase().includes(searchTerm.toLowerCase())
-                            );
-                            return isCategorySelected && isPriceInRange && isTextMatch;
-                        })
-                        .length > 0 ? (
+                <div className="flex-1 h-full flex flex-col justify-center">
+                    <div className="flex-1 h-full flex flex-col justify-center">
                         <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 xl:grid-cols-3 2xl:grid-cols-4 gap-6">
-                            {products.map(product => (
-                                <Card key={product._id} product={product} />
-                            ))}
+                            {products
+                                .filter(product => {
+                                    const productCategoryId = product.category_id._id;
+                                    const isCategorySelected = filteredCategories[productCategoryId] || Object.values(filteredCategories).every(value => !value);
+                                    const isPriceInRange = (
+                                        (!priceFilter.minPrice || product.price >= priceFilter.minPrice) &&
+                                        (!priceFilter.maxPrice || product.price <= priceFilter.maxPrice)
+                                    );
+                                    const isTextMatch = (
+                                        !searchTerm ||
+                                        product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                                        product.description.toLowerCase().includes(searchTerm.toLowerCase())
+                                    );
+                                    return isCategorySelected && isPriceInRange && isTextMatch;
+                                })
+                                .map(product => (
+                                    <Card key={product._id} product={product} />
+                                ))
+                            }
+                            {!products.some(product => {
+                                const productCategoryId = product.category_id._id;
+                                const isCategorySelected = filteredCategories[productCategoryId] || Object.values(filteredCategories).every(value => !value);
+                                const isPriceInRange = (
+                                    (!priceFilter.minPrice || product.price >= priceFilter.minPrice) &&
+                                    (!priceFilter.maxPrice || product.price <= priceFilter.maxPrice)
+                                );
+                                const isTextMatch = (
+                                    !searchTerm ||
+                                    product.name.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                                    product.description.toLowerCase().includes(searchTerm.toLowerCase())
+                                );
+                                return isCategorySelected && isPriceInRange && isTextMatch;
+                            }) && <div className="col-span-full row-span-full h-screen flex items-center justify-center"><span className="text-3xl p-6 text-orange rounded-full font-medium">Results not found!</span></div>}
                         </div>
-                    ) : (
-                        <div className="text-center">There are no products that match the selected filters.</div>
-                    )
-                    }
+                    </div>
                     <Pagination
                         currentPage={currentPage}
                         totalPages={totalPages}
@@ -112,6 +124,7 @@ function Shop() {
                         hasNextPage={hasNextPage}
                         goToPage={goToPage}
                     />
+
                 </div>
 
 
