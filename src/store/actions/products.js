f10f6f2b6955b55f2710bfc879a89840c5fe912f
Gerardo  Aguirre <95187790+Vazhh@users.noreply.github.com>
2023-09-14 17:28:29 -0500

    Mp (#80)
    * paymentForm
    
    * fix
    
    * fixeds
    
    * mp y alerta products

--- src/store/actions/products.js
+++ src/store/actions/products.js
@@ -2,6 +2,7 @@ import { createAction, createAsyncThunk } from "@reduxjs/toolkit";
 import axios from "axios";
 import apiUrl from "../../apiUrl";
 import headers from "../../header";
+import Swal from "sweetalert2";
 
 const save_name = createAction("save_name", (obj) => {
   return { payload: { name: obj.name } };
@@ -13,15 +14,41 @@ const save_checks = createAction("save_checks", (obj) => {
 
 const destroyProduct = createAsyncThunk("destroyProduct", async (obj) => {
   try {
-    let one = await axios.delete(
-      apiUrl + "products/" + obj.product_id,
-      headers()
-    );
-    return {
-      id_to_delete: one.data.response,
-    };
+      const result = await new Promise((resolve) => {
+          Swal.fire({
+              title: 'Are you sure?',
+              text: "You won't be able to revert this!",
+              icon: 'warning',
+              showCancelButton: true,
+              confirmButtonColor: '#3085d6',
+              cancelButtonColor: '#d33',
+              confirmButtonText: 'Yes, delete it!'
+          }).then((result) => {
+              if (result.isConfirmed) {
+                  Swal.fire(
+                      'Deleted!',
+                      'Your file has been deleted.',
+                      'success'
+                  );
+                  resolve(true); // Resuelve la promesa si se confirma la eliminación
+              } else {
+                  resolve(false); // Resuelve la promesa si se cancela la eliminación
+              }
+          });
+      });
+
+      if (result) {
+          const one = await axios.delete(apiUrl + "products/" + obj.product_id, headers());
+          return {
+              id_to_delete: one.data.response
+          };
+      } else {
+          // La eliminación fue cancelada, puedes devolver algo adecuado o lanzar un error.
+          throw new Error('Eliminación cancelada');
+      }
   } catch (error) {
-    return null;
+      // Maneja errores aquí
+      return null;
   }
 });
 
